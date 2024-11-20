0382679aa6ee8212e336c1f1813347db565469d3
Maxim Belkin <belkinurban@gmail.com>
2024-11-20 16:38:01 +0100

    Add initial Vue 3, Vite, and TailwindCSS setup
    This commit introduces the basic configuration for a Vue 3 application using Vite as the build tool and TailwindCSS for styling. It includes necessary config files like `tailwind.config.js`, `postcss.config.js`, `tsconfig` variants, and ignores typical directories and files with `.gitignore`. Additionally, it provides scripts and dependencies in `package.json` for development and testing workflows.

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+import { SmartInput } from '~/features/smart-input';
+
 const userInput = ref('Hi #everyone! What do you think about #this?');
 </script>
 
 <template>
   <div class="app-container">
-    {{ userInput }}
+    <SmartInput v-model="userInput" />
+    <p class="original-text" v-text="userInput || 'Input is empty'" />
   </div>
 </template>
 
-<style lang="postcss" scoped>
+<style scoped lang="postcss">
 .app-container {
   @apply
     flex flex-col gap-3
     justify-center items-center
     h-screen px-20 py-12 lg:px-8;
+}
+.original-text {
+  @apply
+    text-sm text-gray-500;
 }
 </style>
---
+++ src/features/smart-input/ui/SmartInput.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const userInput = defineModel<string>({
+  required: true,
+});
+
+const inputRef = ref<HTMLElement | null>(null);
+
+function onInput (event: Event) {
+  userInput.value = (event.target as HTMLElement).textContent || '';
+}
+</script>
+
+<template>
+  <div
+    ref="inputRef"
+    class="smart-input"
+    role="textbox"
+    contenteditable
+    @input="onInput"
+  >
+    <span v-text="userInput" />
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.smart-input {
+  @apply
+    w-full p-4 border-2 rounded-lg
+    text-gray-500 focus:outline-none focus:border-indigo-400
+    cursor-text;
+}
+</style>
